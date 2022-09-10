d40965612004a267297093008c187db8ace9c78e
Protopoop Aleksandr <alexandr2723@gmail.com>
2022-09-10 22:01:12 +0300

    !

--- js/task-05.js
+++ js/task-05.js
@@ -1,12 +1,23 @@
-10 lines (10 sloc)  365 Bytes
+//10 lines (10 sloc)  365 Bytes
+//
+//const input = document.querySelector('input#name-input');
+//const output = document.querySelector('span#name-output');
+//const handleInput = () => {
+ //   const inputName = input.value.trim();
+ //   if (inputName !== '') {
+ //       return (output.textContent = inputName);
+//    }
+ //   return (output.textContent = 'Anonymous');
+//};
+//input.addEventListener('input', handleInput);
 
-const input = document.querySelector('input#name-input');
-const output = document.querySelector('span#name-output');
-const handleInput = () => {
-    const inputName = input.value.trim();
-    if (inputName !== '') {
-        return (output.textContent = inputName);
+let input = document.getElementById("name-input");
+let nameOutput = document.getElementById("name-output");
+
+input.oninput = function () {
+    if (input.value === '') {
+       nameOutput.innerHTML = 'Anonimus';
+    } else{
+    nameOutput.innerHTML = input.value;
     }
-    return (output.textContent = 'Anonymous');
-};
-input.addEventListener('input', handleInput);
+}
